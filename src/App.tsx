4d6c51c29b59d5712143ef74f022e729e8de6483
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-01-30 16:45:59 +0800

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
+import React from "react"
+
+import Header from "./components/Header"
+
+const App:React.FC = () => {
   return (
-    <>
-    </>
+    <main className="bg-slate-50 min-h-screen w-screen">
+      <Header />
+    </main>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Header:React.FC = () => {
+    return (
+        <header className="w-full border border-b-slate-800 flex items-center border-box p-4">
+            <div className="h-full bg-slate-800 text-slate-50 px-2 py-1">
+                {/* using &nbsp; instead of flexbox & gap to save unnecessary calculations */}
+                <span className="font-bold">Yang-Yi Shen</span> 
+                &nbsp;
+                <span className="font-extralight">/</span> 
+                &nbsp;
+                <span className="font-bold font-serif">沈洋逸</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header
