eae17e0806e53309ee4e82a2d8497872562614f2
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-01-30 19:44:49 +0800

    feat: minor modifications to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Header: React.FC = () => {
     return (
-        <header className="w-full border border-b-slate-800 flex items-center border-box p-4">
-            <div className="h-full bg-slate-800 text-slate-50 px-2 py-1">
+        <header className="w-full border-b border-slate-700 flex items-center border-box p-4">
+            <div className="h-full bg-slate-700 text-slate-50 px-2 py-1">
                 {/* using &nbsp; instead of flexbox & gap to save unnecessary calculations */}
                 <span className="font-bold">Yang-Yi Shen</span>
                 &nbsp;
